513d5f407efce1f400aae07e7761da22ab4f67c7
Shaun Yong <yongfongchuan@gmail.com>
2021-07-10 11:16:12 +1000

    comments and readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # world-health-2020
-Learning to apply data science knowledge and skills by finding proper data representation of various data in World Health Statistics 2020 provided in Kaggles.
+A learning project to apply data science knowledge and skills by finding proper data representation of various data in World Health Statistics 2020 provided in Kaggles.
+
+Created utility functions to inspect data and to clean data for machine learning to predict life expectancy.
--- my_utils.py
+++ my_utils.py
@@ -2,6 +2,9 @@ import os, re
 import numpy as np
 import pandas as pd
 
+# Contains utility functions to be used in jupyter notebooks
+
+# File names of all csv files
 all_files = [
     'lifeExpectancyAtBirth', 'HALElifeExpectancyAtBirth', 'maternalMortalityRatio',
     'birthAttendedBySkilledPersonal', 'infantMortalityRate', 'neonatalMortalityRate',
@@ -19,19 +22,24 @@ all_files = [
 
 class MyUtils():
     def __init__(self):
+        # RE expression for bracketed information within the dataset
         self.rebrackets = re.compile(r'\[[\d.]+\-[\d.]+\]')
     def get_data(self, fname, indicator=False):
+        # Utility function that takes file name and returns a dataframe
         dfnew = pd.read_csv(os.path.join('dataset', f"{fname}.csv"))
         if 'Indicator' in dfnew.columns.tolist() and not indicator:
             dfnew.drop('Indicator', axis=1, inplace=True)
         return dfnew
     def collate_period(self, period):
+        # Collate data for a certain year
         dfnew = pd.DataFrame(columns=['Location'])
         dfnew.set_index('Location', inplace=True)
         for fname in all_files:
             concat_list = [dfnew]
+            # Get raw dataframe
             dff = self.get_data(fname, indicator=True)
             dff = dff[dff['Period'] == period]
+            # Data cleaning
             if 'Dim1' in dfnew.columns.tolist():
                 if 'Both sexes' in set(dff['Dim1']):
                     dff = dff[dff['Dim1'] == 'Both sexes']
@@ -45,9 +53,11 @@ class MyUtils():
                     new_row = pd.DataFrame([[index, row['First Tooltip']]], columns=['Location', row['Indicator']])
                     new_row.set_index('Location', inplace=True)
                     concat_list.append(new_row)
+            # Concatenate data from different csv files
             dfnew = pd.concat(concat_list)
         return dfnew
     def collate_latest(self):
+        # Same as above but takes the latest data of each location instead
         dfnew = pd.DataFrame(columns=['Location'])
         dfnew.set_index('Location', inplace=True)
         for fname in all_files:
@@ -73,6 +83,7 @@ class MyUtils():
             dfnew = pd.concat(concat_list)
         return dfnew
     def remove_brackets(self, numstr):
+        # Utility function to remove brackets from a value if the value is string
         try:
             re_match = self.rebrackets.search(numstr)
             try:
@@ -83,15 +94,18 @@ class MyUtils():
         except TypeError:
             return numstr
     def df_latest_cleaned(self):
+        # Get latest data of each location and cleans it further
         df = self.collate_latest()
         df = df[df.isna().sum(axis=1) < 10]
         cols = df.columns[df.dtypes == np.object].tolist()
         for _, row in df.iterrows():
             for col in cols:
                 df.loc[row.name, col] = self.remove_brackets(row[col])
+        # Fill unknown values with average
         df = df.fillna(df.mean())
         return df
     def df_difference(self, df1, df2, diff, cols=None, set_index=None):
+        # Compare difference of a certain column for DataFrames of different time periods or genders
         if set_index != None:
             df1 = df1.set_index(set_index)
             df2 = df2.set_index(set_index)
@@ -108,6 +122,7 @@ class MyUtils():
                 dfnew = dfnew.append(rowdict, ignore_index=True)
         return dfnew
     def column_lists(self):
+        # Print all column names
         for fname in all_files:
             dff = self.get_data(fname, indicator=True)
             print(dff.columns)
